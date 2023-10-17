b45fb9e6f1e540b94a594b075220ae657172fd70
VigneshEswaramurthi <vignesheswaramurthi@gmail.com>
2023-10-17 17:29:27 +0530

    Add files via upload

---
+++ Day-01/simpleProgram.py
@@ -0,0 +1,9 @@
+print('Vignesh is devOps engineer') #print the text
+
+numberValue=10
+print(numberValue)
+print(type(numberValue))
+
+stringName='Vignesh is devOps engineer'
+print(stringName)
+print(type(stringName))
